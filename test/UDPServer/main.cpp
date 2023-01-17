f72ee3512cb8e69baf2c82513583af0c3e75fc6e
tonytao <158964026@qq.com>
2023-01-17 11:50:13 +0800

    code udp server on windows

---
+++ test/UDPServer/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "../../src/Server.h"
+
+int main()
+{
+	UDPServer server;
+
+	//server.resetIP("127.0.0.1");
+	server.resetPort(9999);
+
+	if (server.connect())
+	{
+		char buf[1024] = { 0 };
+		std::cout << "connect success\n";
+		server.run();
+	}
+}
